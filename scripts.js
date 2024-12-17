da3584189c87efe52332b6207ebb6d7eb531563a
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2024-12-17 15:36:28 -0300

    currency convertion and formatting

--- scripts.js
+++ scripts.js
@@ -1,1 +1,54 @@
-console.log("Script carregado!");
+const USD = 6.08;
+const EUR = 6.38;
+const GBP = 7.73;
+
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const footer = document.querySelector("footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
+
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
+};
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    const exchangeResult = formatCurrency(amount * price).replace("R$", "");
+
+    description.textContent = `${symbol} 1 = R$ ${formatCurrency(price)}`;
+    result.textContent = `${exchangeResult} Reais`;
+
+    footer.classList.add("show-result");
+  } catch (error) {
+    console.log(error);
+    footer.classList.remove("show-result");
+    alert("Não foi possível realizar a conversão, tente novamente mais tarde.");
+  }
+}
+
+function formatCurrency(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
